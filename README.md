b0a8d448ea2ad27a72de91c150057f61cb602e38
Daniel <dte.biotech46@gmail.com>
2026-07-17 09:44:28 -0400

    {Created a 2D array of every nums pairing, logic error fix TBD}

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 My solution to problem #3312 on LeetCode using Java
 
 Algorithm
-1. Create all possible pairings from *nums*
-2. Find the GCD of each pairing
-3. Store each GCD into *gcdPairs*
-4. Sort *gcdPairs* in ascending order
-5. Loop over *queries*
+1. Define a 2D array *numsPairings*
+2. Create all possible pairings from *nums*
+	1. Loop from 0 to length(nums) - 1, using variable *indexI*
+		1. Loop from indexI + 1 to length(nums) -1, using variable *indexJ*
+			1. Put nums[indexI] and nums[indexJ] into numsPairings individually
+3. Find the GCD of each pairing
+4. Store each GCD into *gcdPairs*
+5. Sort *gcdPairs* in ascending order
+6. Loop over *queries*
 	1. Add gcdPairs[queries[i]] to answer[]
-6. Return answer[]
+7. Return answer[]
--- main.java
+++ main.java
@@ -1,5 +1,30 @@
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Hello world");	
+		// Define variables
+
+		int[] nums = {2,3,4};
+		int[] queries = {0,2,2};
+		
+		int[][] numsPairings = new int[100][2];
+
+		int pairCount = 0; // defines the row index in the 2D array where each pair of nums should be placed in
+
+		// Create every combination of nums
+
+		for (int indexI = 0; indexI < nums.length; indexI++) {
+			for (int indexJ = indexI + 1; indexJ < nums.length; indexJ++) {
+				numsPairings[pairCount][0] = indexI;
+				numsPairings[pairCount][1] = indexJ;
+
+				pairCount++;
+			}
+		}
+
+		for (int row = 0; row < numsPairings.length; row++) {
+			for (int column = 0; column < numsPairings[row].length; column++) {
+				System.out.println(numsPairings[row][column]);
+			}
+		}
+		
 	}
 }
